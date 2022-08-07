db2670252f1fff3f5dbb214d4931b8dcc5c0fe4a
JuctTr <1301842163@qq.com>
2022-08-07 15:12:33 +0800

    feat(stack): 用栈模拟队列

--- 2.栈/Stack.js
+++ 2.栈/Stack.js
@@ -1,102 +1,97 @@
-
 class StackBasedArray {
-    constructor () {
-        this.items = []
+    constructor() {
+        this.items = [];
     }
 
-    push (element) {
-        return this.items.push(element)
+    push(element) {
+        return this.items.push(element);
     }
 
-    pop () {
-        return this.items.pop()
+    pop() {
+        return this.items.pop();
     }
 
-    peek () {
-        return this.items[this.size() - 1]
+    peek() {
+        return this.items[this.size() - 1];
     }
 
-    size () {
-        return this.items.length
+    size() {
+        return this.items.length;
     }
 
-    isEmpty () {
-        return this.size() === 0
+    isEmpty() {
+        return this.size() === 0;
     }
 
-    clear () {
-        this.items = []
+    clear() {
+        this.items = [];
     }
 
-    toString () {
-        return this.items.toString()
+    toString() {
+        return this.items.toString();
     }
 }
 
-const stackBasedArray = new StackBasedArray()
-stackBasedArray.push(1)
-stackBasedArray.push(2)
-console.log(stackBasedArray, stackBasedArray.size(), stackBasedArray.peek(), stackBasedArray.toString())
-
-
 class StackBasedObject {
-    constructor () {
-        this.count = 0
-        this.items = {}
+    constructor() {
+        this.count = 0;
+        this.items = {};
     }
 
-    push (element) {
-        this.items[this.count] = element
-        this.count++
+    push(element) {
+        this.items[this.count] = element;
+        this.count++;
     }
 
-    pop () {
-        if (this.isEmpty()) return null
-        this.count--
-        const result = this.items[this.count]
-        delete this.items[this.count]
-        return result
+    pop() {
+        if (this.isEmpty()) return null;
+        this.count--;
+        const result = this.items[this.count];
+        delete this.items[this.count];
+        return result;
     }
 
-    peek () {
-        if (this.isEmpty()) return null
-        return this.items[this.count - 1]
+    peek() {
+        if (this.isEmpty()) return null;
+        return this.items[this.count - 1];
     }
 
-    size () {
-        return this.count
+    size() {
+        return this.count;
     }
 
-    isEmpty () {
-        return this.count === 0
+    isEmpty() {
+        return this.count === 0;
     }
 
-    clear () {
-        this.items = {}
-        this.count = 0
+    clear() {
+        this.items = {};
+        this.count = 0;
         // while (!this.isEmpty()) {
         //     this.pop()
         // }
     }
 
-    toString () {
-        if (this.isEmpty()) return ''
-        let objString = `${this.items[0]}`
+    toString() {
+        if (this.isEmpty()) return '';
+        let objString = `${this.items[0]}`;
         for (let i = 1; i < this.count; i++) {
             objString = `${objString},${this.items[i]}`;
         }
         return objString;
     }
 }
-const stackBasedObject = new StackBasedObject()
-stackBasedObject.push(1)
-stackBasedObject.push(2)
-stackBasedObject.push(4)
-stackBasedObject.push(5)
-stackBasedObject.pop()
-console.log(stackBasedObject, stackBasedObject.size(), stackBasedObject.peek(), stackBasedObject.toString())
+// const stackBasedObject = new StackBasedObject();
+// stackBasedObject.push(1);
+// stackBasedObject.push(2);
+// stackBasedObject.push(4);
+// stackBasedObject.push(5);
+// stackBasedObject.pop();
+// console.log(
+//     stackBasedObject,
+//     stackBasedObject.size(),
+//     stackBasedObject.peek(),
+//     stackBasedObject.toString()
+// );
 
-export {
-    StackBasedArray,
-    StackBasedObject
-}
+export { StackBasedArray, StackBasedObject };
--- 2.栈/stackToQueue.js
+++ 2.栈/stackToQueue.js
@@ -1,61 +1,52 @@
+import { StackBasedArray } from './Stack.js';
 
-import { StackBasedArray } from './Stack.js'
+const stackA = new StackBasedArray();
+const stackB = new StackBasedArray();
 
-
-const stackA = new StackBasedArray()
-const stackB = new StackBasedArray()
-
+/**
+ * @description 用栈模拟队列
+ */
 class stackToQueue {
-    constructor() {
-
-    }
+    constructor() {}
 
     /**
      * 入队操作
      * @param {*} element
      */
-    enQueue (element) {
-        stackA.push(element)
+    enQueue(element) {
+        stackA.push(element);
     }
     /**
      * 出队操作
      */
-    deQueue () {
+    deQueue() {
         if (stackB.isEmpty()) {
             if (stackA.isEmpty()) {
                 return null;
             }
-            this.transfer()
+            this.transfer();
         }
 
         return stackB.pop();
     }
 
-    transfer () {
+    transfer() {
         while (!stackA.isEmpty()) {
             stackB.push(stackA.pop());
         }
     }
-
 }
 
+const stackDemo = new stackToQueue();
 
-const stackDemo = new stackToQueue()
+stackDemo.enQueue(1);
+stackDemo.enQueue(2);
+stackDemo.enQueue(3);
 
-stackDemo.enQueue(1)
-stackDemo.enQueue(2)
-stackDemo.enQueue(3)
+stackDemo.deQueue();
+stackDemo.deQueue();
 
-stackDemo.deQueue()
-stackDemo.deQueue()
+console.log(stackA.items, stackB.items);
 
-console.log(stackA.items, stackB.items)
-
-stackDemo.enQueue(4)
-console.log(stackA.items, stackB.items)
-
-
-
-
-
-
+stackDemo.enQueue(4);
+console.log(stackA.items, stackB.items);
